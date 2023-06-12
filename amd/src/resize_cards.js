5832e7fa92b4e9ca0dfd76ba29cb97598776fbed
think-modular <will.nahmens@think-modular.com>
2023-06-12 17:11:24 +0200

    SW: clean JS implementation

--- amd/src/resize_cards.js
+++ amd/src/resize_cards.js
@@ -1,9 +1,63 @@
-define(['resize_cards'],
-  function($) {
+define(['resize_cards'], function() {
+
+    var SlickResizer = function(slideshow) {                
+        this.show(slideshow)  
+        this.resize(slideshow)              
+                                   
+    };
+
+    SlickResizer.prototype.resize = function(slideshow) {        
+    
+        // Fix height for cards.
+        var cards = slideshow.getElementsByClassName('card')            
+        var height = 0    
+        var heightPx = ""
+        
+        // Get max height.        
+        if (cards) {               
+            for (var i = 0; i < cards.length; i++) {                  
+                if (cards[i].offsetHeight > height) {                       
+                    cards[i].style.minHeight = 0;                
+                    height = cards[i].offsetHeight                    
+                }  
+            }               
+        }   
+
+        // Set min height. For some reason this only works in a separate loop.
+        if (cards) {                    
+            heightPx = height + 'px'        
+            for (var i = 0; i < cards.length; i++) {  
+                cards[i].style.height = heightPx;                
+            }
+        }
+
+        // Fix carousel width inside text labels.
+        var labelFix = slideshow.parentElement.parentElement.parentElement.parentElement
+        if (labelFix) {            
+            if (labelFix.classList.contains('description-inner')) {
+                if (!labelFix.classList.contains('w-100')) {
+                    console.log('Fixing width inside labels for slick-carousel')
+                    labelFix.classList.add('w-100')
+                    labelFix.parentElement.classList.add('p-0')
+                }                
+            }
+        }                
+
+        // Fix carousel height inside text labels.        
+        var totalHeight = height + 100            
+        var totalHeightPx = totalHeight + "px"                
+        slideshow.parentElement.style.height = totalHeightPx         
+
+    }
+
+    
+    SlickResizer.prototype.show = function(slideshow) {
+        slideshow.parentElement.classList.remove('hidden')        
+    };
+    
     return {
-      'init': function() {
-        console.log("resize")
-      }
-    };  
-  }
-)
+        'init': function(slideshow) {                        
+            return new SlickResizer(slideshow);
+        }
+    };
+})
--- amd/src/slick_carousel.js
+++ amd/src/slick_carousel.js
@@ -1,49 +1,50 @@
 define(['jquery', 'slick', 'slick_carousel'],
-  function($) {
-    return {
-      'init': function() {
-        $('.responsive').slick({
-            dots: true,
-            infinite: false,
-            speed: 300,
-            slidesToShow: 5,
-            slidesToScroll: 4,
-            responsive: [
-                {
-                breakpoint: 2000,
-                settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
-                }
-                },
-                {
-                breakpoint: 1600,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 2,
-                    infinite: true,
-                    dots: true
-                }
-                },
-                {
-                breakpoint: 1200,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-                },
-                {
-                breakpoint: 991,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-                },  
-            ]
-        });        
-      }
-    };  
-  }
+    function($) {
+        return {
+        'init': function(carouselId) { 
+            console.log("Initializing slick carousel")       
+            $(carouselId).slick({
+                dots: true,
+                infinite: false,
+                speed: 300,
+                slidesToShow: 5,
+                slidesToScroll: 4,
+                responsive: [
+                    {
+                    breakpoint: 2000,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 3,
+                        infinite: true,
+                        dots: true
+                    }
+                    },
+                    {
+                    breakpoint: 1600,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 2,
+                        infinite: true,
+                        dots: true
+                    }
+                    },
+                    {
+                    breakpoint: 1200,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
+                    },
+                    {
+                    breakpoint: 991,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                    },  
+                ]
+            });        
+        }
+        };  
+    }
 )
