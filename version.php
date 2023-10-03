ded874b60ed16df76509854b853d6a3da2b1fff2
stemiwe <stefan.weber@think-modular.com>
2023-10-03 09:02:24 +0200

    Updated version

--- version.php
+++ version.php
@@ -27,6 +27,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'filter_courselist';
 $plugin->requires  = 2021040100;
 
-$plugin->version   = 2023070201;
-$plugin->release = 'v1.5.4';
+$plugin->version   = 202310031;
+$plugin->release = 'v1.5.5';
 $plugin->maturity = MATURITY_STABLE;
