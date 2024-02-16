134ba3225f59d6bbf4c7fe9cd21988376de4bb7d
think-modular <will.nahmens@think-modular.com>
2024-02-16 11:38:21 +0100

    Upped version

--- version.php
+++ version.php
@@ -27,6 +27,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'filter_courselist';
 $plugin->requires  = 2021040100;
 
-$plugin->version   = 2024012401;
-$plugin->release = 'v1.7.1';
+$plugin->version   = 2024021601;
+$plugin->release = 'v1.7.2';
 $plugin->maturity = MATURITY_STABLE;
