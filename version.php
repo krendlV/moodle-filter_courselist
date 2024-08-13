a536cf4a9e2e9dd9aaaa82422c476ccf3d7c94ee
think-modular <will.nahmens@think-modular.com>
2024-08-13 08:43:21 +0200

    Fix courselist search for subpages with existing URL parameters

--- version.php
+++ version.php
@@ -27,6 +27,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'filter_courselist';
 $plugin->requires  = 2021040100;
 
-$plugin->version   = 2024021601;
-$plugin->release = 'v1.7.2';
+$plugin->version   = 2024081301;
+$plugin->release = 'v1.8';
 $plugin->maturity = MATURITY_STABLE;
