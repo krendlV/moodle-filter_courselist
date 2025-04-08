05882f5cc9f1b05ae957236311665528c47c45dc
stemiwe <stefan.weber@think-modular.com>
2025-04-08 11:41:44 +0200

    Fix anonymous define call in JS

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'filter_courselist';
 $plugin->requires  = 2021040100;
-$plugin->version   = 2025030501;
-$plugin->release = 'v4.5 - 1.9.3';
+$plugin->version   = 2025040801;
+$plugin->release = 'v4.5 - 2.0.0';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->supported = [405, 405];
