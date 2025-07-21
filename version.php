ed3dca5a0b3ce63b63428ed69840b49c40be5d24
sweber <stefan.weber@think-modular.com>
2025-07-21 09:34:54 +0200

    Stop rows in list template from overflowing

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'filter_courselist';
 $plugin->requires  = 2021040100;
-$plugin->version   = 2025062501;
-$plugin->release = 'v4.5 - 2.0.5a';
+$plugin->version   = 2025072101;
+$plugin->release = 'v4.5 - 2.0.5b';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->supported = [405, 405];
