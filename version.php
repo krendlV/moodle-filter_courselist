a23d6089b2ec6e70e11f7214f7f91b39ca323f51
stefan weber <stefan.weber@think-modular.com>
2025-07-23 10:46:59 +0200

    Remove deprecated strings from template

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'filter_courselist';
 $plugin->requires  = 2021040100;
-$plugin->version   = 2025072201;
-$plugin->release = 'v4.5 - 2.1.1';
+$plugin->version   = 2025072301;
+$plugin->release = 'v4.5 - 2.1.2';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->supported = [405, 405];
